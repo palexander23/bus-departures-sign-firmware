a0f3d3528a0a8b2226069f44fff89f6ab0fe8f6f
pete <peter.d.alexander23@gmail.com>
2023-03-08 22:48:09 +0000

    Add class for representing Departure Info
    The arrivals data is returned by the API in this format.
    For convinience the type contains both a text and integer representation
    of the departure.

--- bus-departures-cli/api.py
+++ bus-departures-cli/api.py
@@ -1,6 +1,8 @@
 import requests
 import re
 
+from departure_time_info import DepartureTimeInfo
+
 URL_TEMPLATE = "http://www.cambridgeshirebus.info/Popup_Content/WebDisplay/WebDisplay.aspx?stopRef="
 
 # Bus Stop IDs
@@ -50,6 +52,19 @@ def get_departure_time_strs(departures_html):
     return [re.search(DEPARTURE_TIME_REGEX, line).group(1) for line in departure_lines]
 
 
+def get_departures_dict(departure_lines):
+    """!
+    Convert the strings extracted from HTML to useful representations of data.
+
+    @param departure_lines A list of strs representing departures (e.g. "10 Mins"j, "Due")
+
+    @returns A list of DepartureTimeInfo objects generated from departure_lines
+    """
+
+    return [DepartureTimeInfo(line) for line in departure_lines]
+
+
 if __name__ == "__main__":
     html = get_departure_html(BUSWAY_SHIRE_HALL_N)
-    print(get_departure_time_strs(html))
+    lines = get_departure_time_strs(html)
+    print(get_departures_dict(lines))
