2c82809e53cc525baf57672f41b90da2fc869e86
Pete Alexander <Pete.Alexander@42T.com>
2023-04-26 12:25:33 +0100

    Recreate UI in Rust Version

--- bus-departures-rs/src/main.rs
+++ bus-departures-rs/src/main.rs
@@ -1,23 +1,44 @@
 mod api;
 
-use tabled::{
-    builder::{self, Builder},
-    settings::Style,
-};
+use core::time;
+use std::thread;
+
+use tabled::{builder::Builder, settings::Style};
 
 use api::{get_departures, BUSWAY_SHIRE_HALL_N};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let departures = get_departures(BUSWAY_SHIRE_HALL_N)?;
-    let mut table_builder = Builder::default();
+    let mut prev_table_line_num = 0;
 
-    table_builder.set_header(["Service", "Destination", "Departure"]);
+    loop {
+        let departures = get_departures(BUSWAY_SHIRE_HALL_N)?;
+        let mut table_builder = Builder::default();
 
-    departures.iter().for_each(|dep| {
-        table_builder.push_record([&dep.service, &dep.destination, &dep.time]);
-    });
+        table_builder.set_header(["Service", "Destination", "Departure"]);
 
-    println!("{}", table_builder.build().with(Style::rounded()));
+        departures.iter().for_each(|dep| {
+            table_builder.push_record([&dep.service, &dep.destination, &dep.time]);
+        });
 
-    Ok(())
+        let mut table = table_builder.build();
+        let table = table.with(Style::rounded());
+
+        let table_line_num = table.total_height();
+
+        println!("{}", table);
+
+        thread::sleep(time::Duration::from_secs(2));
+
+        for _ in 0..table_line_num {
+            print!("{}", ansi_escapes::CursorPrevLine);
+        }
+
+        if prev_table_line_num > table_line_num {
+            for _ in 0..prev_table_line_num - table_line_num {
+                println!("{}", ansi_escapes::EraseLine);
+            }
+        }
+
+        prev_table_line_num = table_line_num;
+    }
 }
